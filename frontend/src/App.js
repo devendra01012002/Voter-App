b954dfb550f904192a4c0923c4017ce00e748fcf
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-03-14 07:47:36 +0530

    added signup and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
       <Route path="/" element={< Home/>} />
         <Route path="/Login" element={< Login/>} />
-        <Route path="/Signup" element={< SignUp/>} />
+        <Route path="/signup" element={< SignUp/>} />
         {/* <Route path="/invalid" element={< InvalidPage/>} /> */}
       </Routes>
     </div>
--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Login.css"
+function Login() {
+  const [aadharNumber, setAadharNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
 
-const Login = () => {
+  const handleAadharNumberChange = (event) => {
+    setAadharNumber(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here
+  };
+
   return (
-    <div>Login</div>
-  )
+    
+    <div className="login">
+      <div>
+      <h2>Login</h2>
+      <p>Enter your details to login</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="aadharNumber"
+          placeholder="Aadhar Number"
+          value={aadharNumber}
+          onChange={handleAadharNumberChange}
+        />
+        <input
+          type="text"
+          className="phoneNumber"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+        <input
+          type="password"
+          className="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
